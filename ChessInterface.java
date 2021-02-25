64adf5876cde4f44e68348747102d917021559fb
TyrannicalThesaurus <68403395+TyrannicalThesaurus@users.noreply.github.com>
2021-02-25 10:18:47 -0600

    Add files via upload

---
+++ ChessInterface.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ChessInterface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ChessInterface
+{
+    public static void main(String args[])
+    {
+        //wow cant wait for this to have code lol
+    }
+}
