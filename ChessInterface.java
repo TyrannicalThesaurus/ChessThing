c9d577c4ba9b54827f3f8266152f98e56e757151
TyrannicalThesaurus <68403395+TyrannicalThesaurus@users.noreply.github.com>
2021-02-25 13:07:11 -0600

    Update ChessInterface.java

--- ChessInterface.java
+++ ChessInterface.java
@@ -9,20 +9,28 @@ public class ChessInterface
 {
     public static void main(String args[])
     {
-        //for now this'll be an int array. I could probably make this strings or smth
-        int[][] board = new int[8][8];
+        //for now this'll be an string array
+        String[][] board = new String[8][8];
+        ChessPiece test = new ChessPiece(1,2,"test",1);
         
-        board[1][2] = 3;
-        //
+        //this will get its own method soon
         for(int x = 0; x < board.length; x++)
         {
             String str = "";
             for(int y = 0; y < board.length; y++)
             {
+                if (board[x][y] == null)
+                board[x][y] = " ";
                 str += board[x][y] + ", "; 
             }
             System.out.println(str);
         }
+        
+        
+    }
+    
+    public static void addToBoard(String[][] board, ChessPiece piece) 
+    {
+        board[piece.getLocationx()][piece.getLocationy()] = piece.getName();
     }
 }
-
