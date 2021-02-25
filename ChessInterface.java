38f6ee029d0e85bf400ae7fbd645d2485c4535c0
TyrannicalThesaurus <68403395+TyrannicalThesaurus@users.noreply.github.com>
2021-02-25 10:52:40 -0600

    Update ChessInterface.java

--- ChessInterface.java
+++ ChessInterface.java
@@ -9,6 +9,20 @@ public class ChessInterface
 {
     public static void main(String args[])
     {
-        //wow cant wait for this to have code lol
+        //for now this'll be an int array. I could probably make this
+        int[][] board = new int[8][8];
+        
+        board[1][2] = 3;
+        //
+        for(int x = 0; x < board.length; x++)
+        {
+            String str = "";
+            for(int y = 0; y < board.length; y++)
+            {
+                str += board[x][y] + ", "; 
+            }
+            System.out.println(str);
+        }
     }
 }
+
