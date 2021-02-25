a9a61fdd7e3d63e08fa918c2e761fed13da875f5
TyrannicalThesaurus <68403395+TyrannicalThesaurus@users.noreply.github.com>
2021-02-25 13:07:57 -0600

    Update ChessPiece.java

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,41 +1,47 @@
 class ChessPiece
 {
- private int[] myLocation; //x and y cord 
- private String myName; //name for the piece ie(king, bishop)
- private int pointValue; //amount of points a piece is worth (pawn = 1, bishop = 3, etc)
- private String myColor; //assigns either black or white to the piece
- 
- public ChessPiece(int[] l, String n, int p)
- {
-   myLocation = l;
-   myName = n;
-   pointValue = p;
- }
- 
- public int[] getLocation()
- {
-   return myLocation;
- }
- 
-  public String getName()
- {
-   return myName;
- }
- 
- public int getPoint()
- {
-   return pointValue;
- }
- 
- public String getColor()
- {
-  return myColor;
- }
- 
- public void setLocation(int[] pos)
- {
-     myLocation = pos;
- }
- 
- public abstract void movePiece();
+    private int myLocationx; //x coord 
+    private int myLocationy; //y coord
+    private String myName; //name for the piece ie(king, bishop)
+    private int pointValue; //amount of points a piece is worth (pawn = 1, bishop = 3, etc)
+    private String myColor; //assigns either black or white to the piece
+
+    public ChessPiece(int lx, int ly, String n, int p)
+    {
+        myLocationx = lx;
+        myLocationy = ly;
+        myName = n;
+        pointValue = p;
+    }
+
+    public int getLocationx()
+    {
+        return myLocationx;
+    }
+    
+    public int getLocationy()
+    {
+        return myLocationy;
+    }
+
+    public String getName()
+    {
+        return myName;
+    }
+
+    public int getPoint()
+    {
+        return pointValue;
+    }
+
+    public String getColor()
+    {
+        return myColor;
+    }
+
+    public void setLocation(int row, int col)
+    {
+        myLocationx = row;
+        myLocationy = col;
+    }
 }
