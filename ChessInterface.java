906ca6e014ca491664b737539ce4c360d2a36c7d
TyrannicalThesaurus <68403395+TyrannicalThesaurus@users.noreply.github.com>
2021-02-27 18:32:27 -0600

    Update ChessInterface.java

--- ChessInterface.java
+++ ChessInterface.java
@@ -9,22 +9,22 @@ public class ChessInterface
 {
     public static void main(String args[])
     {
-        //for now this'll be an string array
+        //for now this'll be a string array
         String[][] board = new String[8][8];
-        ChessPiece test = new ChessPiece(1,2,"test",1);
+        Pawn test = new Pawn(3,2,"test",1);
         
-        //this will get its own method soon
-        for(int x = 0; x < board.length; x++)
-        {
-            String str = "";
-            for(int y = 0; y < board.length; y++)
-            {
-                if (board[x][y] == null)
-                board[x][y] = " ";
-                str += board[x][y] + ", "; 
-            }
-            System.out.println(str);
-        }
+        addToBoard(board,test);
+        pause(1000);
+        test.moveForward();
+        addToBoard(board,test);
+        
+        pause(1000);
+        test.moveForward();
+        addToBoard(board,test);
+        
+        pause(1000);
+        test.moveForward();
+        addToBoard(board,test);
         
         
     }
@@ -32,5 +32,34 @@ public class ChessInterface
     public static void addToBoard(String[][] board, ChessPiece piece) 
     {
         board[piece.getLocationx()][piece.getLocationy()] = piece.getName();
+        refresh(board);
+    }
+    
+    public static void refresh(String[][] board)
+    {
+        System.out.println('\u000C');
+        for(int y = 0; y < board.length; y++)
+        {
+            String str = "";
+            for(int x = 0; x < board.length; x++)
+            {
+                if (board[x][y] == null)
+                board[x][y] = " ";
+                str += board[x][y] + ", "; 
+            }
+            System.out.println(str);
+        }
+    }
+    
+    public static void pause(int n)
+    {
+        try
+        {
+            Thread.sleep(n);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
